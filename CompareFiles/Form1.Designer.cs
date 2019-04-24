b7fcd12b2d2b47b089b30041bdfda8c5250f9bc3
Chandra Sekhar Ravuri <chandrasekhar.r18@gmail.com>
2019-04-24 22:34:37 +0530

    Added source code

---
+++ CompareFiles/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace CompareFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.compareButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.file2Label = new System.Windows.Forms.Label();
+            this.file1Lable = new System.Windows.Forms.Label();
+            this.vsLoactionlabel = new System.Windows.Forms.Label();
+            this.vsPathTextBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // compareButton
+            // 
+            this.compareButton.Location = new System.Drawing.Point(351, 401);
+            this.compareButton.Name = "compareButton";
+            this.compareButton.Size = new System.Drawing.Size(95, 30);
+            this.compareButton.TabIndex = 0;
+            this.compareButton.Text = "Compare";
+            this.compareButton.UseVisualStyleBackColor = true;
+            this.compareButton.Click += new System.EventHandler(this.compareButton_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.file2Label, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.file1Lable, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(2, 35);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(792, 360);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // file2Label
+            // 
+            this.file2Label.AllowDrop = true;
+            this.file2Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.file2Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.file2Label.Location = new System.Drawing.Point(399, 0);
+            this.file2Label.Name = "file2Label";
+            this.file2Label.Size = new System.Drawing.Size(390, 360);
+            this.file2Label.TabIndex = 1;
+            this.file2Label.Text = "File 2";
+            this.file2Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.file2Label.DragDrop += new System.Windows.Forms.DragEventHandler(this.file2Label_DragDrop);
+            this.file2Label.DragOver += new System.Windows.Forms.DragEventHandler(this.file2Label_DragOver);
+            // 
+            // file1Lable
+            // 
+            this.file1Lable.AllowDrop = true;
+            this.file1Lable.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.file1Lable.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.file1Lable.Location = new System.Drawing.Point(3, 0);
+            this.file1Lable.Name = "file1Lable";
+            this.file1Lable.Size = new System.Drawing.Size(390, 360);
+            this.file1Lable.TabIndex = 0;
+            this.file1Lable.Text = "File 1";
+            this.file1Lable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.file1Lable.DragDrop += new System.Windows.Forms.DragEventHandler(this.file1Lable_DragDrop);
+            this.file1Lable.DragOver += new System.Windows.Forms.DragEventHandler(this.file1Lable_DragOver);
+            // 
+            // vsLoactionlabel
+            // 
+            this.vsLoactionlabel.AutoSize = true;
+            this.vsLoactionlabel.Location = new System.Drawing.Point(0, 7);
+            this.vsLoactionlabel.Name = "vsLoactionlabel";
+            this.vsLoactionlabel.Size = new System.Drawing.Size(123, 17);
+            this.vsLoactionlabel.TabIndex = 2;
+            this.vsLoactionlabel.Text = "Visual Studio Path";
+            // 
+            // vsPathTextBox
+            // 
+            this.vsPathTextBox.Location = new System.Drawing.Point(130, 7);
+            this.vsPathTextBox.Name = "vsPathTextBox";
+            this.vsPathTextBox.Size = new System.Drawing.Size(658, 22);
+            this.vsPathTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 433);
+            this.Controls.Add(this.vsPathTextBox);
+            this.Controls.Add(this.vsLoactionlabel);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.compareButton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Compare Files";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button compareButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label file2Label;
+        private System.Windows.Forms.Label file1Lable;
+        private System.Windows.Forms.Label vsLoactionlabel;
+        private System.Windows.Forms.TextBox vsPathTextBox;
+    }
+}
+
---
+++ CompareFiles/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompareFiles
+{
+    public partial class Form1 : Form
+    {
+        private string file1Path;
+        private string file2Path;
+        private string vsPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void file2Label_DragOver(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void file2Label_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = e.Data.GetData(DataFormats.FileDrop) as string[];
+            if(files != null && files.Any())
+            {
+                file2Path = files.First();
+                this.file2Label.Text = file2Path;
+            }
+        }
+
+        private void file1Lable_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void file1Lable_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = e.Data.GetData(DataFormats.FileDrop) as string[];
+            if (files != null && files.Any())
+            {
+                file1Path = files.First();
+                this.file1Lable.Text = file1Path;
+            }
+        }
+
+        private void compareButton_Click(object sender, EventArgs e)
+        {
+            vsPath = GetVisualStudioPath();
+            
+            if (!string.IsNullOrEmpty(file1Path) && !string.IsNullOrEmpty(file2Path) && vsPath != "")
+            {
+                string exeString = "\"" + vsPath + $@"\devenv.exe" + "\"";
+                string argString = $"/diff \"{file1Path}\" \"{file2Path}\"";
+                Process.Start(exeString, argString);
+            }
+        }
+
+        private string GetVisualStudioPath()
+        {
+            string vsPath = this.vsPathTextBox.Text;
+            if (string.IsNullOrEmpty(vsPath))
+            {
+                vsPath = ConfigurationManager.AppSettings["VSPath"];
+            }
+
+            return vsPath;
+        }
+    }
+}
